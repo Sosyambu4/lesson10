78a9f0f9a529d75310a7463d1674b3a1b7142e5f
Sosyambu4 <vladislavhrabryk@gmail.com>
2021-06-30 22:14:24 +0200

    const [state, dispatch] = useReducer(reducer, initialArg, init);

---
+++ src/state/user-reducer.test.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {userReducer} from './user-reducer';
+import {v1} from "uuid";
+
+
+let startState = { age: 20, childrenCount: 2, name: 'Dimych' };
+
+let endState = userReducer(startState, { type: 'INCREMENT-AGE' })
+
+beforeEach(() => {
+    startState = { age: 20, childrenCount: 2, name: 'Dimych' };
+
+    endState = userReducer(startState, { type: 'INCREMENT-AGE' })
+})
+
+
+test('user reducer should increment only age', () => {
+    /*const startState = { age: 20, childrenCount: 2, name: 'Dimych' };
+
+    const endState = userReducer(startState, { type: 'INCREMENT-AGE' })*/
+
+    expect(endState.age).toBe(21);
+    expect(endState.childrenCount).toBe(2);
+});
+
+test('user reducer should increment only childrenCount', () => {
+    /*const startState = { age: 20, childrenCount: 2, name: 'Dimych' };
+    const endState = userReducer(startState, { type: 'INCREMENT-CHILDREN-COUNT' })
+*/
+    expect(endState.age).toBe(20);
+    expect(endState.childrenCount).toBe(3);
+});
+
+test('user reducer should change name of user', () => {
+    const newName = 'Viktor';
+    const startState = { name: 'Dimych', age: 20, childrenCount: 2 };
+    const endState = userReducer(startState, { type: 'CHANGE-NAME', newName: newName })
+
+    expect(endState.name).toBe(newName);
+});
+
+
